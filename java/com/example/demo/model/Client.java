6ece969b87328063b5f96afef7ff1760c2b69e64
szkolenia-arkadiusz-sas <105731948+szkolenia-arkadiusz-sas@users.noreply.github.com>
2023-04-06 18:00:09 +0200

    Add files via upload

--- java/com/example/demo/model/Client.java
+++ java/com/example/demo/model/Client.java
@@ -1,11 +1,13 @@
 package com.example.demo.model;
 
+import com.example.demo.annotation.PostalCode;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @NoArgsConstructor
@@ -22,4 +24,6 @@ public class Client {
 
     private String firstName;
 
+    @PostalCode
+    private String postalCode;
 }
